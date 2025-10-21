3d88fb4af75ce49bd80a11ccb73a50cfa953c3e6
Isamu <isamu.dy2@gmail.com>
2025-10-21 13:34:20 -0700

    Add .NET Standard core library and test project structure

---
+++ ClueGame.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClueGame.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ ClueGame.Core/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ClueGame.Core
+{
+    public class Class1
+    {
+
+    }
+}
