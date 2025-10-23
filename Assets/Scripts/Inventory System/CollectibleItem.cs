150cd8fdb2c7268006fa02f1cf6b25a9b3c3525e
hmoss49 <hollymoss49@gmail.com>
2025-10-22 18:59:32 -0700

    Inventory, Accusations, and Secret Passages Working

--- Assets/Scripts/Inventory System/CollectibleItem.cs
+++ Assets/Scripts/Inventory System/CollectibleItem.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 public class CollectibleItem : MonoBehaviour
 {
     public string itemName;
-    public string eventId;
 
-    public void PickUp() { }
+    public void PickUp()
+    {
+        InventoryManager.Instance.AddItemToInventory(itemName);
+        Destroy(gameObject);
+    }
 }
